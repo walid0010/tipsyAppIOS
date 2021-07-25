acee95184f0bfe5d6905784f7578e1804b7e4aa0
walid0010 <walidchekir98@gmail.com>
2021-07-25 16:54:23 +0100

    Initial Commit

---
+++ tipsyAppIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipsyAppIOS
+//
+//  Created by chekir walid on 25/7/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
